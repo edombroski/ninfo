a2512de719d6da2e8f04b810028ead674f999aea
Eric Dombroski <eric@edombroski.com>
2021-02-13 11:11:43 -0500

    Support hostnames in cymruwhois plugin

--- ninfo/plugins/cymruwhois_plugin.py
+++ ninfo/plugins/cymruwhois_plugin.py
@@ -1,21 +1,26 @@
 from ninfo import PluginBase
+import socket
 import cymruwhois
 
+
 class cymru_whois(PluginBase):
-    """This plugin returns the owners name and ASN of this ip"""
+    """This plugin returns the owners name and ASN of this host"""
 
     name = 'cymruwhois'
     title = 'Cymru Whois'
     description = 'Cymru Whois lookup'
-    types = ['ip', 'ip6']
+    types = ['ip', 'ip6', 'hostname']
     local = False
 
     def setup(self):
         self.c = cymruwhois.Client()
 
-    def get_info(self, ip):
-        #Don't bother looking up on-campus hosts
-        info = self.c.lookup(ip)
-        return info.__dict__
+    def get_info(self, target):
+        try:
+            ip = socket.gethostbyname(target)
+            info = self.c.lookup(ip)
+            return info.__dict__
+        except socket.gaierror:
+            return dict(asn="",cc="",prefix="",owner="Host not found.")
 
 plugin_class = cymru_whois
